f483c91363a39cc63a7894e8572cc114ac665dc8
Landon <donovandelint.09@hotmail.com>
2025-10-22 11:04:32 -0600

    1.0.0

---
+++ models/server.js
@@ -0,0 +1,59 @@
+// models/server.js
+import express from "express";
+
+class Server {
+  constructor() {
+    this.app = express();
+    this.port = process.env.PORT;
+
+    //middlewares
+    this.middlewares();
+    //rutas de mi aplicacion
+    this.routes();
+  }
+
+  middlewares() {
+    //directorio publico
+    this.app.use(express.static("public"));
+  }
+
+  routes() {
+    this.app.get("/api", (req, res) => {
+      res.json({
+        msg: "get API",
+      });
+    });
+
+    this.app.put("/api", (req, res) => {
+      res.json({
+        msg: "put API",
+      });
+    });
+
+    this.app.post("/api", (req, res) => {
+      res.json({
+        msg: "post API",
+      });
+    });
+
+    this.app.delete("/api", (req, res) => {
+      res.json({
+        msg: "delete API",
+      });
+    });
+
+    this.app.patch("/api", (req, res) => {
+      res.json({
+        msg: "patch API",
+      });
+    });
+  }
+
+  listen() {
+    this.app.listen(this.port, () => {
+      console.log("Servidor corriendo en puerto", this.port);
+    });
+  }
+}
+
+export default Server;
